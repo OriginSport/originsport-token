fa042775d2161efda6eaba25ddcbb74ed8488c88
XatMassacrE <xxat.massacre@gmail.com>
2018-04-19 02:17:35 +0800

    Add burn function and update some properties

--- contracts/OriginSportToken.sol
+++ contracts/OriginSportToken.sol
@@ -7,61 +7,105 @@ import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
 contract OriginSportToken is StandardToken, Ownable {
   using SafeMath for uint;
 
+  // Events
+  event Burn(address indexed _burner, uint _value);
+
   // Constants
-  string public name   = 'OriginSport';
-  string public symbol = 'ORS';
-  uint public constant decimal = 10**uint(18);
-  uint public constant TOTAL_SUPPLY        = 300000000 * decimal;
-  uint public constant CROWDSALE_ALLOWANCE = 180000000 * decimal;
-  uint public constant ADMIN_ALLOWANCE     = 120000000 * decimal;
+  string public name                = 'Origin Sport Token';
+  string public symbol              = 'ORS';
+  uint public constant decimal      = 18;
 
   // Properties
-  uint public crowdSaleAllowance;      // the number of tokens available for crowdsales
-  uint public adminAllowance;          // the number of tokens available for the administrator
-  address public crowdSaleAddr;           // the address of a crowdsale currently selling this token
-  address public adminAddr;               // the address of a crowdsale currently selling this token
-
+  uint public totalSupply           = 300000000 * 10 ** uint(decimal);
+  uint public transferableStartTime;
+  address public  tokenSaleContract;
 
   // Filter invalid address
   modifier validAddress(address addr) {
     require(addr != address(0x0));
     require(addr != address(this));
-    require(addr != owner);
-    require(addr != address(adminAddr));
-    require(addr != address(crowdSaleAddr));
+    _;
+  }
+
+  modifier onlyWhenTransferEnabled() {
+    if (now < transferableStartTime) {
+      require(msg.sender == tokenSaleContract || msg.sender == owner);
+    }
     _;
   }
 
   /**
-   * @dev Constructor for Origin Sport token
-   * @dev Assigns the total supply to admin address 
+   * @dev Constructor for Origin Sport Token, assigns the total supply to admin address 
+   * @param _transferableStartTime the time ors can transfer
+   * @param _admin the admin address of ors
    */
-  function OriginSportToken(address addr) public {
-    balances[addr] = TOTAL_SUPPLY;
+  function OriginSportToken(uint _transferableStartTime, address _admin) public {
+    balances[msg.sender] = totalSupply;
+    Transfer(address(0x0), msg.sender, totalSupply);
+
+    transferableStartTime = _transferableStartTime;
+    tokenSaleContract = msg.sender;
+
+    transferOwnership(_admin);
   }
 
   /**
-   * Overrides transfer function with modifier to prevents
-   * from transfer with invalid address
+   * @dev overrides transfer function with modifier to prevent from transfer with invalid address
+   * @param _to The address to transfer to
+   * @param _value The amount to be transferred
    */
-  function transfer(address _to, uint _value) public validAddress(_to) returns (bool) {
+  function transfer(address _to, uint _value) public 
+    validAddress(_to) 
+    onlyWhenTransferEnabled 
+    returns (bool) 
+  {
     return super.transfer(_to, _value);
   }
 
   /**
-   * Overrides transferFrom function with modifier to prevents
-   * from transfer with invalid address
+   * @dev overrides transfer function with modifier to prevent from transfer with invalid address
+   * @param _from The address to transfer from.
+   * @param _to The address to transfer to.
+   * @param _value The amount to be transferred.
    */
-  function transferFrom(address _from, address _to, uint _value) public validAddress(_to) returns (bool) {
-    require(_value <= balances[_from]);
-    require(_value <= allowed[_from][msg.sender]);
+  function transferFrom(address _from, address _to, uint _value) public 
+    validAddress(_to) 
+    onlyWhenTransferEnabled 
+    returns (bool) 
+  {
+    return super.transferFrom(_from, _to, _value);
+  }
 
-    bool result = super.transferFrom(_from, _to, _value);
-    if (result) {
-      balances[_from] = balances[_from].sub(_value);
-      balances[_to] = balances[_to].add(_value);
-      allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
-    }
-    return result;
+  /**
+   * @dev burn tokens
+   * @param _value The amount to be burned.
+   * @return always true (necessary in case of override)
+   */
+  function burn(uint _value) public onlyWhenTransferEnabled returns (bool) {
+    balances[msg.sender] = balances[msg.sender].sub(_value);
+    totalSupply = totalSupply.sub(_value);
+    Burn(msg.sender, _value);
+    Transfer(msg.sender, address(0x0), _value);
+    return true;
+  }
+
+  /**
+   * @dev burn tokens in the behalf of someone
+   * @param _from The address of the owner of the token.
+   * @param _value The amount to be burned.
+   * @return always true (necessary in case of override)
+   */
+  function burnFrom(address _from, uint _value) public onlyWhenTransferEnabled returns(bool) {
+    require(transferFrom(_from, msg.sender, _value));
+    return burn(_value);
+  }
+
+  /**
+   * @dev transfer to owner any tokens send by mistake on this contracts
+   * @param token The address of the token to transfer.
+   * @param amount The amount to be transfered.
+   */
+  function emergencyERC20Drain(ERC20 token, uint amount) public onlyOwner {
+    token.transfer(owner, amount);
   }
 }

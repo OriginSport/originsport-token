2536b4306032346994ec93858672d3ab48046254
XatMassacrE <xxat.massacre@gmail.com>
2018-04-19 02:19:22 +0800

    Add some Date functions to deploy token sale

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,30 @@
-var OriginSportToken = artifacts.require("./OriginSportToken.sol");
-var OriginSportDistribution = artifacts.require("./OriginSportDistribution.sol");
+const OriginSportToken = artifacts.require("./OriginSportToken.sol")
+const OriginSportTokenSale = artifacts.require("./OriginSportTokenSale.sol")
+
+Date.prototype.getUnixTime = function() { 
+  return this.getTime()/1000|0
+}
+
+if(!Date.now) {
+  Date.now = function() { 
+    return new Date()
+  }
+}
+
+Date.time = function() { 
+  return Date.now().getUnixTime()
+}
 
 module.exports = function(deployer, network, accounts) {
-  deployer.deploy(OriginSportToken, accounts[0]).then(function() {
-    return deployer.deploy(OriginSportDistribution, new Date()/1000);
-  });
-};
+  const publicSaleStartTime = new Date("Tue, 15 May 2018 08:00:00 GMT").getUnixTime()
+  const publicSaleEndTime = new Date("Wed, 30 May 2018 08:00:00 GMT").getUnixTime()
+  console.log('-------------------------------------------------')
+	console.log('publicSaleStartTime : ' + publicSaleStartTime)
+	console.log('publicSaleStartTime : ' + new Date(publicSaleStartTime*1000))
+	console.log('publicSaleEndTime   : ' + publicSaleEndTime)
+	console.log('publicSaleEndTime   : ' + new Date(publicSaleEndTime*1000))
+	console.log('-------------------------------------------------')
+  deployer.deploy(OriginSportToken, publicSaleEndTime, accounts[0]).then(function() {
+    return deployer.deploy(OriginSportTokenSale, publicSaleStartTime, publicSaleEndTime)
+  })
+}

b017e45267865a3446294d97a57baa933732e2c3
XatMassacrE <xxat.massacre@gmail.com>
2018-05-14 16:15:21 +0800

    Add batchTransfer help users send ors more efficiently

--- contracts/OriginSportToken.sol
+++ contracts/OriginSportToken.sol
@@ -92,6 +92,23 @@ contract OriginSportToken is StandardToken, Ownable, BurnableToken {
   }
 
   /**
+   * @dev overrides transfer function with modifier to prevent from transfer with invalid address
+   * @param _recipients An array of address to transfer to.
+   * @param _value The amount to be transferred.
+   */
+  function batchTransfer(address[] _recipients, uint _value) public onlyWhenTransferable returns (bool) {
+    uint count = _recipients.length;
+    require(count > 0 && count <= 20);
+    uint needAmount =  count.mul(_value);
+    require(_value > 0 && balances[msg.sender] >= needAmount);
+
+    for (uint i = 0; i < count; i++) {
+      transfer(_recipients[i], _value);
+    }
+    return true;
+  }
+
+  /**
    * @dev overrides burn function with modifier to prevent burn while untransferable
    * @param _value The amount to be burned.
    */

bfe29e1cc5ebc10d88a6e8a1b585078526ff5d6f
XatMassacrE <xxat.massacre@gmail.com>
2018-05-23 18:30:50 +0800

    Update migrate files to deploy to production

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -13,10 +13,12 @@ module.exports = function(deployer, network, accounts) {
   // Address for test
   const tokenAddress = '0xf25186b5081ff5ce73482ad761db0eb0d25abfbf'
   const walletAddress = '0x627306090abaB3A6e1400e9345bC60c78a8BEf57'
+  const testAdminAddr = '0x00c6bFFba6eD9EA434AcB096D171C0a78e24D318'
  
-  // Deploy with new OriginSportToken
-  // OriginSportToken.new(publicSaleEndTime, accounts[1]).then(function(instance) {
-  deployer.deploy(OriginSportToken, accounts[1]).then(function(instance) {
-    return deployer.deploy(OriginSportTokenSale, publicSaleStartTime, publicSaleEndTime, tokenAddress, walletAddress)
-  })
+  //Deploy with new OriginSportToken
+  //OriginSportToken.new(publicSaleEndTime, accounts[1]).then(function(instance) {
+  //deployer.deploy(OriginSportToken, accounts[1]).then(function(instance) {
+  //  return deployer.deploy(OriginSportTokenSale, publicSaleStartTime, publicSaleEndTime, tokenAddress, walletAddress)
+  //})
+  deployer.deploy(OriginSportToken, testAdminAddr)
 }
